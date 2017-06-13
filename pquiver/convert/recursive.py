c7eff207f7289f0d00101d4ddbce2035ad21f293
Jim Pivarski <jpivarski@gmail.com>
2017-06-13 16:38:53 +0200

    start porting recursive counters

---
+++ pquiver/convert/recursive.py
@@ -0,0 +1,112 @@
+from pquiver.typesystem.defs import *
+from pquiver.typesystem.np import *
+from pquiver.typesystem.lrup import *
+from pquiver.typesystem.naming import *
+
+def declare(tpe, prefix, sizetype=uint64, tagtype=uint8):
+    def modifiers(tpe, name):
+        if tpe.optional:
+            name = name.optional()
+        if tpe.rtname is not None:
+            name = name.runtime(tpe.rtname)
+        return name
+
+    def recurse(tpe, name, sizename):
+        if isinstance(tpe, Primitive):
+            if sizename is None:
+                return {str(modifiers(tpe, name)): tpe.dtype}
+            else:
+                return {str(modifiers(tpe, name)): tpe.dtype, str(sizename.size()): sizetype}
+
+        elif isinstance(tpe, List):
+            name = modifiers(tpe, name).list()
+            return recurse(tpe.items, name, name)
+
+        elif isinstance(tpe, Record):
+            out = {}
+            for n, t in tpe.fields.items():
+                out.update(recurse(t, modifiers(tpe, name).field(n), sizename))
+            return out
+
+        else:
+            assert False, "unrecognized type: {0}".format(tpe)
+
+    return recurse(tpe, ArrayName(prefix), None)
+
+def extracttype(dtypes, prefix):
+    def modifiers(name):
+        optional = name.isoptional
+        if name.isoptional:
+            name = name.dropoptional()
+        rtname = None
+        if name.isruntime:
+            rtname, name = name.dropruntime()
+        return name, optional, rtname
+
+    def recurse(dtypes):
+        assert len(dtypes) > 0
+
+        check = {}
+        trimmed = {}
+        for n, d in dtypes.items():
+            n, optional, rtname = modifiers(n)
+            islist = n.islist
+            isrecord = n.isrecord
+
+            if "optional" in check: assert optional == check["optional"]
+            else: check["optional"] = optional
+
+            if "rtname" in check: assert rtname == check["rtname"]
+            else: check["rtname"] = rtname
+
+            if "islist" in check: assert islist == check["islist"]
+            else: check["islist"] = islist
+
+            if "isrecord" in check: assert isrecord == check["isrecord"]
+            else: check["isrecord"] = isrecord
+
+            if islist:
+                n = n.droplist()
+                trimmed[n] = d
+
+            elif isrecord:
+                fname, n = n.dropfield()
+                trimmed[fname] = trimmed.get(fname, {})
+                trimmed[fname][n] = d
+
+            else:
+                trimmed[n] = d
+
+        assert not (islist and isrecord)
+
+        if not islist and not isrecord:
+            assert len(trimmed) == 1
+            (n, d), = trimmed.items()
+            out = identifytype(Primitive(d))
+
+        elif islist:
+            out = List(recurse(trimmed))
+
+        elif isrecord:
+            out = Record(**dict((n, recurse(dts)) for n, dts in trimmed.items()))
+
+        if optional:
+            out = Optional(out)
+
+        if rtname is None:
+            return out
+        else:
+            return Type.materialize(rtname, out)
+
+    parsed = {}
+    for n, d in dtypes.items():
+        p = ArrayName.parse(prefix, n)
+        if p is not None:
+            if p.ispage:
+                p = p.droppage()
+            if not p.issize and not p.istag:
+                parsed[p] = d
+
+    return recurse(parsed)
+
+
--- pquiver/typesystem/np.py
+++ pquiver/typesystem/np.py
@@ -30,38 +30,12 @@ complex128 = PrimitiveWithRepr(numpy.dtype("complex128"), repr="complex128")
 complex256 = PrimitiveWithRepr(numpy.dtype("complex256"), repr="complex256")
 
 def identifytype(primitive):
-    if primitive.generic == "Primitive" and primitive.rtname is None:
-        if primitive.dtype == boolean.dtype:
-            return Optional(boolean) if primitive.optional else boolean
-        elif primitive.dtype == int8.dtype:
-            return Optional(int8) if primitive.optional else int8
-        elif primitive.dtype == int16.dtype:
-            return Optional(int16) if primitive.optional else int16
-        elif primitive.dtype == int32.dtype:
-            return Optional(int32) if primitive.optional else int32
-        elif primitive.dtype == int64.dtype:
-            return Optional(int64) if primitive.optional else int64
-        elif primitive.dtype == uint8.dtype:
-            return Optional(uint8) if primitive.optional else uint8
-        elif primitive.dtype == uint16.dtype:
-            return Optional(uint16) if primitive.optional else uint16
-        elif primitive.dtype == uint32.dtype:
-            return Optional(uint32) if primitive.optional else uint32
-        elif primitive.dtype == uint64.dtype:
-            return Optional(uint64) if primitive.optional else uint64
-        elif primitive.dtype == float32.dtype:
-            return Optional(float32) if primitive.optional else float32
-        elif primitive.dtype == float64.dtype:
-            return Optional(float64) if primitive.optional else float64
-        elif primitive.dtype == float128.dtype:
-            return Optional(float128) if primitive.optional else float128
-        elif primitive.dtype == complex64.dtype:
-            return Optional(complex64) if primitive.optional else complex64
-        elif primitive.dtype == complex128.dtype:
-            return Optional(complex128) if primitive.optional else complex128
-        elif primitive.dtype == complex256.dtype:
-            return Optional(complex256) if primitive.optional else complex256
-        else:
-            return primitive
-    else:
-        return primitive
+    if primitive.optional:
+        return Optional(identifytype(primitive.type))
+
+    elif primitive.rtname is None:
+        for tpe in [boolean, int8, int16, int32, int64, uint8, uint16, uint32, uint64, float32, float64, float128, complex64, complex128, complex256]:
+            if primitive.dtype == tpe.dtype:
+                return tpe
+
+    return primitive
