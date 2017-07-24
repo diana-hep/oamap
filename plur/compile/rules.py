59195d47a253bda44581724129eb5e0a8e56e396
Jim Pivarski <jpivarski@gmail.com>
2017-07-24 12:05:39 -0500

    start type inference

---
+++ plur/compile/rules.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+# Copyright 2017 DIANA-HEP
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+# Add ()
+# alias ("name", "asname")
+# And ()
+# arg ("arg", "annotation") # Py3 only
+# arguments ("args", "vararg", "kwarg", "defaults")                               # Py2
+# arguments ("args", "vararg", "kwonlyargs", "kw_defaults", "kwarg", "defaults")  # Py3
+# Assert ("test", "msg")
+# Assign ("targets", "value")
+# Attribute ("value", "attr", "ctx")
+# AugAssign ("target", "op", "value")
+# AugLoad ()
+# AugStore ()
+# BinOp ("left", "op", "right")
+# BitAnd ()
+# BitOr ()
+# BitXor ()
+# BoolOp ("op", "values")
+# Break ()
+# Bytes ("s",)  # Py3 only
+# Call ("func", "args", "keywords", "starargs", "kwargs")
+# ClassDef ("name", "bases", "body", "decorator_list")                                   # Py2
+# ClassDef ("name", "bases", "keywords", "starargs", "kwargs", "body", "decorator_list") # Py3
+# Compare ("left", "ops", "comparators")
+# comprehension ("target", "iter", "ifs")
+# Continue ()
+# Del ()
+# Delete ("targets",)
+# DictComp ("key", "value", "generators")
+# Dict ("keys", "values")
+# Div ()
+# Ellipsis ()
+# Eq ()
+# ExceptHandler ("type", "name", "body")
+# Exec ("body", "globals", "locals") # Py2 only
+# Expression ("body",)
+# Expr ("value",)
+# ExtSlice ("dims",)
+# FloorDiv ()
+# For ("target", "iter", "body", "orelse")
+# FunctionDef ("name", "args", "body", "decorator_list")             # Py2
+# FunctionDef ("name", "args", "body", "decorator_list", "returns")  # Py3
+# GeneratorExp ("elt", "generators")
+# Global ("names",)
+# Gt ()
+# GtE ()
+# IfExp ("test", "body", "orelse")
+# If ("test", "body", "orelse")
+# ImportFrom ("module", "names", "level")
+# Import ("names",)
+# In ()
+# Index ("value",)
+# Interactive ("body",)
+# Invert ()
+# Is ()
+# IsNot ()
+# keyword ("arg", "value")
+# Lambda ("args", "body")
+# ListComp ("elt", "generators")
+# List ("elts", "ctx")
+# Load ()
+# LShift ()
+# Lt ()
+# LtE ()
+# Mod ()
+# Module ("body",)
+# Mult ()
+# NameConstant ("value",)  # Py3 only
+# Name ("id", "ctx")
+# Nonlocal ("names",)  # Py3 only
+# Not ()
+# NotEq ()
+# NotIn ()
+# Num ("n",)
+# Or ()
+# Param ()
+# Pass ()
+# Pow ()
+# Print ("dest", "values", "nl")  # Py2 only
+# Raise ("type", "inst", "tback")  # Py2
+# Raise ("exc", "cause")           # Py3
+# Repr ("value",)  # Py2 only
+# Return ("value",)
+# RShift ()
+# SetComp ("elt", "generators")
+# Set ("elts",)
+# Slice ("lower", "upper", "step")
+# Starred ("value", "ctx")  # Py3 only
+# Store ()
+# Str ("s",)
+# Sub ()
+# Subscript ("value", "slice", "ctx")
+# Suite ("body",)
+# TryExcept ("body", "handlers", "orelse")         # Py2 only
+# TryFinally ("body", "finalbody")                 # Py2 only
+# Try ("body", "handlers", "orelse", "finalbody")  # Py3 only
+# Tuple ("elts", "ctx")
+# UAdd ()
+# UnaryOp ("op", "operand")
+# USub ()
+# While ("test", "body", "orelse")
+# withitem ("context_expr", "optional_vars")      # Py3 only
+# With ("context_expr", "optional_vars", "body")  # Py2
+# With ("items", "body")                          # Py3
+# Yield ("value",)
