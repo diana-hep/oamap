5799776e3db46feeddec1db93572e01c846ffa40
Jim Pivarski <jpivarski@gmail.com>
2017-08-31 17:44:03 +0200

    correctly interpreting the new NanoAOD with nested records

--- nanoaod2.py
+++ nanoaod2.py
@@ -27,6 +27,9 @@ for leaf in leaves:
 
 packages = {}
 for leaf in leaves:
+    if len(leaf.GetBranch().GetListOfLeaves()) != 1:
+        raise TypeError("TLeaf \"{0}\" is not the only leaf in TBranch \"{1}\"".format(leaf.GetName(), leaf.GetBranch().GetName()))
+
     path = leaf.GetName().split("_")
 
     pack = packages
@@ -37,25 +40,25 @@ for leaf in leaves:
 
     pack[path[-1]] = leaf
 
-def toplurtype(pack, allowcounters=True):
+def toplurtype(pack, name, allowcounters):
     if not isinstance(pack, dict):
         typename = pack.GetTypeName()
         if typename == "Char_t":
-            tpe = int8
+            tpe = withrepr(int8, copy=True)
         elif typename == "UChar_t":
-            tpe = uint8
+            tpe = withrepr(uint8, copy=True)
         elif typename == "Int_t":
-            tpe = int32
+            tpe = withrepr(int32, copy=True)
         elif typename == "UInt_t":
-            tpe = uint32
+            tpe = withrepr(uint32, copy=True)
         elif typename == "Long64_t":
-            tpe = int64
+            tpe = withrepr(int64, copy=True)
         elif typename == "ULong64_t":
-            tpe = uint64
+            tpe = withrepr(uint64, copy=True)
         elif typename == "Float_t":
-            tpe = float32
+            tpe = withrepr(float32, copy=True)
         elif typename == "Double_t":
-            tpe = float64
+            tpe = withrepr(float64, copy=True)
         else:
             raise NotImplementedError, typename
 
@@ -63,6 +66,9 @@ def toplurtype(pack, allowcounters=True):
         check = re.sub("/[CBbSsIiFDLlo]$", "", title)
         if check != pack.GetName() and check != pack.GetName().split("_")[-1]:
             tpe.help = title
+
+        tpe.column = name.str()
+        tpe.branchname = pack.GetBranch().GetName()
         return tpe
 
     else:
@@ -85,22 +91,39 @@ def toplurtype(pack, allowcounters=True):
         for counter, pairs in bycounter.items():
             if counter is None:
                 for item, data in pairs:
-                    fields[item] = toplurtype(data, allowcounters=allowcounters)
+                    fields[item] = toplurtype(data, name.toRecord(item), allowcounters)
 
             else:
-                name = counter
-                if name.startswith("n"):
-                    name = name[1:]
+                n = counter
+                if n.startswith("n"):
+                    n = n[1:]
 
                 subfields = {}
                 for item, data in pairs:
-                    subfields[item] = toplurtype(data, allowcounters=False)
+                    if len(bycounter) == 1:
+                        subname = name.toListData().toRecord(item)
+                    else:
+                        subname = name.toRecord(n).toListData().toRecord(item)
+
+                    subfields[item] = toplurtype(data, subname, False)
+                    counterbranch = data.GetLeafCount().GetBranch()
                 
-                fields[name] = List(Record(**subfields))
+                fields[n] = List(Record(**subfields))
+                fields[n].of.column = None
+                fields[n].of.branchname = None
+                fields[n].column = name.toListOffset().str()
+                fields[n].branchname = counterbranch.GetName()
 
         if len(fields) == 1:
             return list(fields.values())[0]
         else:
-            return Record(**fields)
+            out = Record(**fields)
+            out.column = None
+            out.branchname = None
+            return out
 
-print formattype(toplurtype(packages))
+tpe = List(toplurtype(packages, ArrayName("events").toListData(), True))
+tpe.column = ArrayName("events").toListOffset().str()
+tpe.branchname = None
+
+print formattype(tpe)
