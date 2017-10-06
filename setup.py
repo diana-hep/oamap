da57abf3c0860c75e3d045281aa80fb01422ce76
Jim Pivarski <jpivarski@gmail.com>
2017-10-06 09:08:13 -0500

    description in setup

--- setup.py
+++ setup.py
@@ -35,34 +35,46 @@ from setuptools import setup
 
 def get_version():
     g = {}
-    exec(open(os.path.join("plur", "version.py")).read(), g)
+    exec(open(os.path.join("arrowed", "version.py")).read(), g)
     return g["__version__"]
 
-setup(name = "plur",
+setup(name = "arrowed",
       version = get_version(),
       packages = find_packages(exclude = ["tests"]),
-      scripts = ["scripts/avro2plur"],
-      description = "",
-      long_description = """""",
+      scripts = [],
+      description = "Compiler and proxies for algorithms running on Arrow data, including nested lists.",
+      long_description = """Arrowed provides an object-oriented view of data in `Arrow format <https://arrow.apache.org/`_, including fully nested object hierarchies with arbitrary-length lists. It can also operate on buffers (disconnected arrays) that are formatted like Arrow but not yet assembled into a single data structure, which allows the same data to be used in multiple datasets.
+
+Any function that depends on Arrow data and `the types Numba can handle <http://numba.pydata.org/numba-doc/dev/reference/numpysupported.html>`_ can be "Arrowed". An Arrowed function operates on Arrow buffers in-place, without materializing the objects that you reference in your code. This lets you write natural algorithms, apparently unboxing nested, arbitrary-length lists of record structures, but the only operations that are performed at runtime are contiguous array lookups. Thus, the Arrowed function usually runs much faster than a naive compilation.
+
+This toolkit also includes methods for accessing data interactively in the form of lazy-evaluated proxies. These proxies behave like Python objects; you can interact with them on the commandline or in a notebook, but they load data from Arrow buffers on-the-fly. Compared with compilation, this is a slower but more convenient interface for debugging.
+
+In addition, there are tools for type-inferring and constructing Arrow buffers from Python objects, JSON, etc. and saving these buffers in any format the supports sets of named arrays, such as Numpy .npz files, HDF5, object store databases, etc. The data's schema is encoded in the names of the arrays, and the data are in the arrays.""",
       author = "Jim Pivarski (DIANA-HEP)",
       author_email = "pivarski@fnal.gov",
       maintainer = "Jim Pivarski (DIANA-HEP)",
       maintainer_email = "pivarski@fnal.gov",
-      url = "",
-      download_url = "",
-      license = "Apache Software License v2",
+      url = "https://github.com/diana-hep/arrowed",
+      download_url = "https://github.com/diana-hep/arrowed/releases",
+      license = "BSD 3-clause",
       test_suite = "tests",
-      install_requires = ["numpy"],
+      install_requires = ["numpy", "numba"],  # , "meta"
       tests_require = [],
       classifiers = [
-          "Development Status :: 1 - Planning",
+          "Development Status :: 2 - Pre-Alpha"
           "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
           "Intended Audience :: Science/Research",
-          "License :: OSI Approved :: Apache Software License",
+          "License :: OSI Approved :: BSD License",
           "Operating System :: MacOS",
           "Operating System :: POSIX",
           "Operating System :: Unix",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
           "Topic :: Scientific/Engineering",
           "Topic :: Scientific/Engineering :: Information Analysis",
           "Topic :: Scientific/Engineering :: Mathematics",
