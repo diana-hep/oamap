c15786e78277bbaf72d4d489a16b479678740e3d
Jim Pivarski <jpivarski@gmail.com>
2017-11-29 14:13:03 -0600

    updated long description, then remembered that I need histograms for next Friday

--- setup.py
+++ setup.py
@@ -46,13 +46,9 @@ setup(name = "oamap",
       description = "Toolset for computing directly on hierarchically nested, columnar data, such as Apache Arrow.",
       long_description = """Large datasets can be more compact and faster to access when they are laid out in columns (see `Apache Arrow <https://arrow.apache.org/>`_). Even hierarchically nested data can be presented this way, though converting the data between the columnar form and the object form can degrade performance. Non-hierarchical data (rectangular tables) is often accessed without materializing rows (see `Apache Drill <https://drill.apache.org/docs/performance/>`_), but this is more complex for data containing arbitrary-length lists of objects.
 
-OAMap is a suite of tools for performing calculations in this way. The name stands for Object-Array-Map, in analogy with Object-Relational-Mapping in databases. OAMap has a compiler for high throughput calculations and object proxies for low latency interactive exploration.
+OAMap is a suite of tools for performing calculations in this way. The name stands for Object-Array-Map, in analogy with Object-Relational-Mapping (ORM) in relational databases. Pure Python calculations are considerably faster and more memory efficient when datasets are expressed in OAMaps, but the real power comes from *compiling* columized code. This toolset includes `extensions to Numba <http://numba.pydata.org/numba-doc/dev/extending/index.html>`_ that will compile your object-oriented code into native array manipulations. Generally, you'd use uncompiled Python for low latency exploration of the data and Numba-compiled functions for high throughput.
 
-The compiler takes a Python function operating on objects and converts it to the equivalent function operating on columnar data in Numpy arrays. This by itself can speed up access by an order of magnitude, but especially when further compiled by Numba. Since the transformed function references only numbers and arrays, Numba is capable of compiling it in "nopython" mode.
-
-The proxies are Python objects whose contents and attributes are generated on demand, accessing the columnar arrays, minimizing downloads if the data are remote. Often, it is useful to explore the data interactively in a Python commandline or notebook, then wrap up scratch work as functions to compile. Compiled functions can also return proxies, so a high-speed search may result in anomalies to investigate by hand.
-
-In addition, OAMap contains tools for type-inferring and constructing Arrow buffers from Python objects, JSON, etc., and saving these buffers in any format the supports sets of named arrays, such as Numpy .npz files, HDF5, object store databases, etc. The data's schema is encoded in the names of the arrays, and the data are in the arrays.""",
+OAMap only strictly depends on Numpy, but `Numba <http://numba.pydata.org/>`_ will accelerate it and `pyarrow <https://arrow.apache.org/docs/python/index.html>`_, `h5py <http://www.h5py.org/>`_, etc. provide hooks for converting data among various formats.""",
       author = "Jim Pivarski (DIANA-HEP)",
       author_email = "pivarski@fnal.gov",
       maintainer = "Jim Pivarski (DIANA-HEP)",
@@ -61,7 +57,7 @@ In addition, OAMap contains tools for type-inferring and constructing Arrow buff
       download_url = "https://github.com/diana-hep/oamap/releases",
       license = "BSD 3-clause",
       test_suite = "tests",
-      install_requires = ["numpy", "numba"],  # , "meta"
+      install_requires = ["numpy"],
       tests_require = [],
       classifiers = [
           "Development Status :: 2 - Pre-Alpha"
