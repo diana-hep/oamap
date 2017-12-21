d13ab869eaed20dfc4a742774c74638bfe664d84
Jim Pivarski <jpivarski@gmail.com>
2017-12-21 11:23:41 -0600

    different types of Records have their own sets of valid attributes (good)

--- testy.py
+++ testy.py
@@ -26,15 +26,18 @@ from oamap.schema import *
 #     for j in (0, 1, -1, -2):
 #         print do2(x, i, j), sys.getrefcount(x._arrays), sys.getrefcount(x._cache)
 
-schema = List(Record({"a": Primitive("i8"), "b": Primitive("f8")}))
-x = schema({"object-B": numpy.array([0], dtype=numpy.int32), "object-E": numpy.array([5], dtype=numpy.int32), "object-L-Fa": numpy.array([5, 4, 3, 2, 1]), "object-L-Fb": numpy.array([1.1, 2.2, 3.3, 4.4, 5.5])})
+schema1 = List(Record({"a": Primitive("i8"), "b": Primitive("f8")}))
+x = schema1({"object-B": numpy.array([0], dtype=numpy.int32), "object-E": numpy.array([5], dtype=numpy.int32), "object-L-Fa": numpy.array([5, 4, 3, 2, 1]), "object-L-Fb": numpy.array([1.1, 2.2, 3.3, 4.4, 5.5])})
+
+schema2 = Record({"c": Primitive("i8"), "d": Primitive("f8")})
+y = schema2({"object-Fc": numpy.array([999]), "object-Fd": numpy.array([3.14])})
 
 @numba.njit
-def do(x, i):
-    return x[i].a, x[i].b
+def do(x, i, y):
+    return x[i].a, x[i].b, y.c, y.d
 
-print do(x, 0)
-print do(x, 1)
-print do(x, 2)
-print do(x, 3)
-print do(x, 4)
+print do(x, 0, y)
+print do(x, 1, y)
+print do(x, 2, y)
+print do(x, 3, y)
+print do(x, 4, y)
