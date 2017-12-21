ede16fd0bf3c57ef9112d722e18d435c4e2db8cb
Jim Pivarski <jpivarski@gmail.com>
2017-12-20 19:14:35 -0600

    yes, we don't need cache to be a pointer anymore (so each List is a flat 56 bytes of 7 integers)

--- testy.py
+++ testy.py
@@ -13,7 +13,15 @@ x = schema({"object-B": numpy.array([0], numpy.int32), "object-E": numpy.array([
 # x = schema({"object-B": numpy.array([0], numpy.int32), "object-E": numpy.array([5], numpy.int32), "object-L": numpy.array([1.1, 2.2, 3.3, 4.4, 5.5])})
 
 @numba.njit
-def do(x, i, j):
+def do1(x, i):
+    return x[i]
+
+@numba.njit
+def do2(x, i, j):
     return x[i][j]
 
-print do(x, -1, 0)
+for i in (0, 2, -1, -3):
+    y = do1(x, i)
+    print y, sys.getrefcount(y._arrays), sys.getrefcount(y._cache)
+    for j in (0, 1, -1, -2):
+        print do2(x, i, j), sys.getrefcount(x._arrays), sys.getrefcount(x._cache)
