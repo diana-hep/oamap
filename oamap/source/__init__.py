f0d2894f994fde7c8462763a1a62425fd1d053dc
Jim Pivarski <jpivarski@gmail.com>
2018-01-08 17:16:20 -0600

    [skip ci] to take on Parquet, I'll need a progression of increasingly complex samples

--- oamap/source/__init__.py
+++ oamap/source/__init__.py
@@ -28,6 +28,10 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
+
+
+
 # import pyarrow.parquet
 
 # # schema = pyarrow.parquet.read_schema("/home/pivarski/diana/oamap-gh-pages/examples/planets_formats/planets.parquet")
@@ -40,3 +44,37 @@
 # reader.metadata.schema.column(245)
 # reader.read_column(245)
 # reader.read_column(245).to_pandas()
+
+
+
+
+# import parquet
+# import gzip
+# import io
+# import numpy
+
+# file = open("/home/pivarski/diana/oamap-gh-pages/examples/planets_formats/planets.parquet")
+# if not parquet._check_header_magic_bytes(file) or not parquet._check_footer_magic_bytes(file):
+#     raise IOError("not a valid parquet file (missing magic bytes)")
+# footer = parquet._read_footer(file)
+# footer.num_rows
+# footer.schema   # list
+# footer.key_value_metadata[0]  # .key .value (both unicode) Spark's JSON schema
+# footer.schema[5]  # radialvelocity.val
+# footer.schema[5].type == parquet.parquet_thrift.Type.FLOAT
+# footer.schema[5].repetition_type == parquet.parquet_thrift.FieldRepetitionType.OPTIONAL
+# footer.row_groups[0].columns[3].meta_data.path_in_schema
+# footer.row_groups[0].num_rows
+# footer.row_groups[0].columns[3]
+# footer.row_groups[0].columns[3].file_offset
+
+# file.seek(footer.row_groups[0].columns[3].file_offset)
+# page_header = parquet._read_page_header(file)
+# compresseddata = file.read(page_header.compressed_page_size)
+# data = gzip.GzipFile(fileobj=io.BytesIO(compresseddata), mode="rb").read()
+
+# num_nonnull = page_header.data_page_header.num_values - page_header.data_page_header.statistics.null_count
+# values = numpy.frombuffer(data[-num_nonnull*4:], dtype="<f4")
+
+# page_header.data_page_header.definition_level_encoding == parquet.parquet_thrift.Encoding.RLE
+# page_header.data_page_header.repetition_level_encoding == parquet.parquet_thrift.Encoding.BIT_PACKED
